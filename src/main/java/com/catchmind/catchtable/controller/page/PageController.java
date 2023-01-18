1cc953893acacaf9fb71417614349d2cbc711cd1
dongjinkimbig <kdjkdj11@naver.com>
2023-01-18 18:38:13 +0900

    시큐리티 적용한 로그인, 헤더 타임리프 변경 완료

--- src/main/java/com/catchmind/catchtable/controller/page/PageController.java
+++ src/main/java/com/catchmind/catchtable/controller/page/PageController.java
@@ -1,13 +1,23 @@
 package com.catchmind.catchtable.controller.page;
 
+import com.catchmind.catchtable.service.ProfileLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("")
 public class PageController {
+
+    @Autowired
+    private ProfileLogicService profileLogicService;
 
     @GetMapping("")
     public ModelAndView index() {
@@ -17,6 +27,19 @@ public class PageController {
     @GetMapping("login")
     public ModelAndView login() {
         return new ModelAndView("/login");
+    }
+
+    @PostMapping(path="/loginOk")   // http://localhost:8889/loginOk
+    public String loginOk(HttpServletRequest request, String prHp, String prUserpw){
+        if(profileLogicService.read(prHp, prUserpw).getData() != null){
+            HttpSession session = request.getSession();
+            Long prIdx = profileLogicService.read(prHp, prUserpw).getData().getPrIdx();
+            session.setAttribute("prIdx", prIdx);
+            session.setAttribute("prHp", prHp);
+            return "redirect:/index";
+        }else{
+            return "redirect:/login";
+        }
     }
 
     @GetMapping("join")
